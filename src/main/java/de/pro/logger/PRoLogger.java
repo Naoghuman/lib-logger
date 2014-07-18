c3aa60b65ee4974b14efc31fb84fc5426be70edf
PRo <peter.rogge@yahoo.de>
2014-07-18 21:14:46 +0200

    Initial commit

---
+++ src/main/java/de/pro/logger/PRoLogger.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2014 PRo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.pro.logger;
+
+import de.pro.logger.api.ILogger;
+import java.util.HashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author PRo
+ */
+public final class PRoLogger implements ILogger {
+    
+    private final HashMap<Class, Logger> loggers = new HashMap<>();
+    
+    public PRoLogger() { }
+    
+    private Logger getLogger(Class clazz) {
+        // Look if the class have a logger
+        if (loggers.containsKey(clazz)) {
+            return loggers.get(clazz);
+        }
+        
+        // Create new logger
+        final Logger logger = LogManager.getLogger(clazz);
+        loggers.put(clazz, logger);
+        
+        return logger;
+    }
+
+    @Override
+    public void debug(Class clazz, String msg) {
+        this.debug(clazz, msg, null);
+    }
+
+    @Override
+    public void debug(Class clazz, String msg, Throwable ta) {
+        this.getLogger(clazz).debug(msg, ta);
+    }
+
+    @Override
+    public void error(Class clazz, String msg) {
+        this.error(clazz, msg, null);
+    }
+
+    @Override
+    public void error(Class clazz, String msg, Throwable ta) {
+        this.getLogger(clazz).error(msg, ta);
+    }
+
+    @Override
+    public void info(Class clazz, String msg) {
+        this.info(clazz, msg, null);
+    }
+
+    @Override
+    public void info(Class clazz, String msg, Throwable ta) {
+        this.getLogger(clazz).info(msg, ta);
+    }
+
+    @Override
+    public void warn(Class clazz, String msg) {
+        this.warn(clazz, msg, null);
+    }
+
+    @Override
+    public void warn(Class clazz, String msg, Throwable ta) {
+        this.getLogger(clazz).warn(msg, ta);
+    }
+
+    @Override
+    public void sayWelcome(String welcomeMessage) {
+        final StringBuilder msg = new StringBuilder();
+        msg.append("# "); // NOI18N
+        msg.append(welcomeMessage);
+        if (welcomeMessage.length() < 65) {
+            for (int i = welcomeMessage.length(); i < 65; i++) {
+                msg.append(" "); // NOI18N
+            }
+        }
+        msg.append(" #"); // NOI18N
+        
+        this.getLogger(PRoLogger.class).info("#####################################################################"); // NOI18N
+        this.getLogger(PRoLogger.class).info(msg.toString());
+        this.getLogger(PRoLogger.class).info("#####################################################################"); // NOI18N
+    
+    }
+
+    @Override
+    public void sayGoodbye(String goodbyeMessage) {
+        final StringBuilder msg = new StringBuilder();
+        msg.append("# "); // NOI18N
+        msg.append(goodbyeMessage);
+        if (goodbyeMessage.length() < 65) {
+            for (int i = goodbyeMessage.length(); i < 65; i++) {
+                msg.append(" "); // NOI18N
+            }
+        }
+        msg.append(" #"); // NOI18N
+        
+        
+        this.getLogger(PRoLogger.class).info("#####################################################################"); // NOI18N
+        this.getLogger(PRoLogger.class).info(msg.toString());
+        this.getLogger(PRoLogger.class).info("#####################################################################"); // NOI18N
+    }
+}
---
+++ src/main/java/de/pro/logger/api/ILogger.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2014 PRo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.pro.logger.api;
+
+/**
+ *
+ * @author PRo
+ */
+public interface ILogger {
+    public void debug(Class clazz, String msg);
+    public void debug(Class clazz, String msg, Throwable ta);
+    public void error(Class clazz, String msg);
+    public void error(Class clazz, String msg, Throwable ta);
+    public void info(Class clazz, String msg);
+    public void info(Class clazz, String msg, Throwable ta);
+    public void warn(Class clazz, String msg);
+    public void warn(Class clazz, String msg, Throwable ta);
+    
+    /**
+     * This method print a <code>Welcome</code> message at start in the logfile. 
+     * The length from the <code>param welcomeMessage</code> shouldn't above 65 
+     * characters.<br />
+     * The <code>Welcome</code> message will print in the format:<br /><br />
+     * 
+     * #####################################################################<br />
+     * # Here you can read your welcome message&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#<br />
+     * #####################################################################<br /><br />
+     * 
+     * If your <code>Welcome</code> message is longer then 65 characters then you 
+     * you will see:<br /><br />
+     * 
+     * #####################################################################<br />
+     * # Here you can read your to long ..................... welcome message #<br />
+     * #####################################################################
+     * 
+     * @param welcomeMessage the welcome message
+     */
+    public void sayWelcome(String welcomeMessage);
+    
+    /**
+     * This method print a <code>Goodbye</code> message at start in the logfile. 
+     * The length from the <code>param goodbyeMessage</code> shouldn't above 65 
+     * characters.<br />
+     * The <code>Goodbye</code> message will print in the format:<br /><br />
+     * 
+     * #####################################################################<br />
+     * # Here you can read your goodbye message&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#<br />
+     * #####################################################################<br /><br />
+     * 
+     * If your <code>Goodbye</code> message is longer then 65 characters then you 
+     * you will see:<br /><br />
+     * 
+     * #####################################################################<br />
+     * # Here you can read your to long ..................... goodbye message #<br />
+     * #####################################################################
+     * 
+     * @param goodbyeMessage the goodbye message
+     */
+    public void sayGoodbye(String goodbyeMessage);
+}
