e1ca1fbad493efd9806428cf98ad978ac9f8bc31
PRo <peter.rogge@yahoo.de>
2014-07-26 07:11:23 +0200

    Add JavaDoc to the packages.

---
+++ src/main/java/de/pro/lib/logger/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the <code>API</code> for the library <code>Lib-Logger</code>.
+ */
+package de.pro.lib.logger.api;
---
+++ src/main/java/de/pro/lib/logger/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the implementation for the library <code>Lib-Logger</code>.
+ */
+package de.pro.lib.logger;
